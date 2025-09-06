a6d130d98b5fd5120612b9611a6590e72bdaffcb
bbantu <brianbaraza40@gmail.com>
2025-09-06 16:46:11 +0300

    Refactor transform_data function to improve comments and clarify data cleaning steps(drop cabin column

--- transformation/transform.py
+++ transformation/transform.py
@@ -3,9 +3,13 @@ import pandas as pd
 def transform_data():
     df = pd.read_csv("../raw_titanic.csv")
 
+    # If Age is missing for a passenger in a certain class 
+    # it gets filled with the median age of all in that class.
     df["Age"] = df.groupby(["Sex", "Pclass"])["Age"].transform(
     lambda x: x.fillna(x.median())
 )
+    # drop Cabin (too many missing values)
+    df.drop(columns=["Cabin"], inplace=True)
 
     # Save cleaned dataset
     df.to_csv("../clean_titanic.csv", index=False)
