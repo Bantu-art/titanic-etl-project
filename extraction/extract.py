027f624339593f082a84083037b5741481e6321c
bbantu <brianbaraza40@gmail.com>
2025-09-06 17:45:48 +0300

    Fix file path for saving extracted Titanic dataset

--- extraction/extract.py
+++ extraction/extract.py
@@ -3,7 +3,7 @@ import pandas as pd
 def extract_data():
     url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
     df = pd.read_csv(url)
-    df.to_csv("../raw_titanic.csv", index=False)
+    df.to_csv("../data/raw_titanic.csv", index=False)
     print("✅ Data extracted and saved as raw_titanic.csv")
 
 if __name__ == "__main__":
