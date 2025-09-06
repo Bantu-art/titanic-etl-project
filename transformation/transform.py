54df0420e3b7cf6abdb6ff4667bca10ea55bd0d9
bbantu <brianbaraza40@gmail.com>
2025-09-06 17:02:44 +0300

    Enhance data transformation by filling missing 'Embarked' values and extracting 'Surname' from 'Name' column

--- transformation/transform.py
+++ transformation/transform.py
@@ -11,6 +11,23 @@ def transform_data():
     # drop Cabin (too many missing values)
     df.drop(columns=["Cabin"], inplace=True)
 
+    # Since there are only two missing Embarked coulumn, I am filling
+    # them with the most common
+    df["Embarked"] = df["Embarked"].fillna(df["Embarked"].mode()[0])
+
+    # For clean read I have moved surname from the name column and 
+    # created a new column for it
+    df["Surname"] = df["Name"].str.split(",").str[0].str.strip()
+
+    # For a clean name read I have also moved Surname column to before Name
+    col_index = df.columns.get_loc("Name")  
+    surname_col = df.pop("Surname")          
+    df.insert(col_index, "Surname", surname_col)  
+
+    # strip the name column of the surname and the comma
+    df["Name"] = df["Name"].str.split(",").str[1].str.strip()
+
+
     # Save cleaned dataset
     df.to_csv("../clean_titanic.csv", index=False)
     print("Data transformed and saved as clean_titanic.csv")
