1a04949ab3a123d092d2c46cbd50d2dbdb52868e
bbantu <brianbaraza40@gmail.com>
2025-09-06 16:39:40 +0300

    Add data transformation script for Titanic dataset

---
+++ transformation/transform.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def transform_data():
+    df = pd.read_csv("../raw_titanic.csv")
+
+    df["Age"] = df.groupby(["Sex", "Pclass"])["Age"].transform(
+    lambda x: x.fillna(x.median())
+)
+
+    # Save cleaned dataset
+    df.to_csv("../clean_titanic.csv", index=False)
+    print("Data transformed and saved as clean_titanic.csv")
+
+    # Show start and end of the DataFrame
+    print("\n--- First 5 rows ---")
+    print(df.head(10))
+    print("\n--- Last 5 rows ---")
+    print(df.tail())
+
+if __name__ == "__main__":
+    transform_data()
