df1441852b5a01166a6de328fd2ad43341492bde
bbantu <brianbaraza40@gmail.com>
2025-09-06 17:48:15 +0300

    Fix file path for loading clean Titanic dataset

--- loading/load.py
+++ loading/load.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3
 
 def load_data():
-    df = pd.read_csv("../clean_titanic.csv")
+    df = pd.read_csv("../data/clean_titanic.csv")
     conn = sqlite3.connect("../data/titanic.db")
     df.to_sql("titanic_passengers", conn, if_exists="replace", index=False)
     conn.close()
