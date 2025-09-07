c7b279ea13632a578b6b7693aa109d7da9b46d8b
bbantu <brianbaraza40@gmail.com>
2025-09-08 01:22:53 +0300

    Refactor load_data function to use absolute paths for loading datasets

--- loading/load.py
+++ loading/load.py
@@ -2,8 +2,10 @@ import pandas as pd
 import sqlite3
 
 def load_data():
-    df = pd.read_csv("../data/clean_titanic.csv")
-    conn = sqlite3.connect("../data/titanic.db")
+    # Use absolute paths
+    data_dir = "/home/bantu/dataProjects/titanic_pipeline/data"
+    df = pd.read_csv(f"{data_dir}/clean_titanic.csv")
+    conn = sqlite3.connect(f"{data_dir}/titanic.db")
     df.to_sql("titanic_passengers", conn, if_exists="replace", index=False)
     conn.close()
     print("✅ Data loaded into titanic.db")
