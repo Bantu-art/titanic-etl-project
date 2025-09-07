dc42b1dbcf13d0d5dd6900c87ec992ecb429d5c4
bbantu <brianbaraza40@gmail.com>
2025-09-08 01:22:25 +0300

    Refactor data extraction to use absolute path for saving raw Titanic dataset

--- extraction/extract.py
+++ extraction/extract.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import os
 
 def extract_data():
     url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
     df = pd.read_csv(url)
-    df.to_csv("../data/raw_titanic.csv", index=False)
+    
+    # Use absolute path
+    data_dir = "/home/bantu/dataProjects/titanic_pipeline/data"
+    os.makedirs(data_dir, exist_ok=True)
+    df.to_csv(f"{data_dir}/raw_titanic.csv", index=False)
     print("✅ Data extracted and saved as raw_titanic.csv")
 
 if __name__ == "__main__":
